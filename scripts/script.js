e7e9f8c86cb9a684f3f15075448b3831d18b51eb
rryyii <ryanyi455@gmail.com>
2024-10-27 07:48:10 -0400

    Created search html page and css file, started on script

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,16 @@
 /*
     This is the baseline javascript file for now.
 */
+
+let items = document.getElementsByClassName("premade-playlist");
+let button = document.querySelector("button");
+
+button.addEventListener("click", () => {
+    for (let i = 0; i < items.length; i++) {
+        if (items[i].style.height == "150px") {
+            items[i].style.height = "350px";
+        } else {
+            items[i].style.height = "150px";
+        }
+    }
+});
