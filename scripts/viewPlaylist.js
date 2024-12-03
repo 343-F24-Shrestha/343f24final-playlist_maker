22bc8bed046635464efaa5405643a00b6c43c827
JoePlato <guns.m.lyon@gmail.com>
2024-12-03 14:53:16 -0500

    Fixed a bug where debug was showing up on the playlist display

--- scripts/viewPlaylist.js
+++ scripts/viewPlaylist.js
@@ -3,8 +3,10 @@ const playlistDisplay = document.getElementById("playlist-list");
 // Iterates through the localStorage to search for non-token playlists
 for (let i = 0; i < localStorage.length; i++) {
         if (localStorage.key(i) != "spotify_access_token") {
-                const playlist = document.createElement("li");
-                playlist.textContent = localStorage.key(i);
-                playlistDisplay.appendChild(playlist);
+                if(localStorage.key(i) != "debug") {
+                        const playlist = document.createElement("li");
+                        playlist.textContent = localStorage.key(i);
+                        playlistDisplay.appendChild(playlist);
+                }
         }
 }
