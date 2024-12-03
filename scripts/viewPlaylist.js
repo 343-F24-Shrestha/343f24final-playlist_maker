23fa260a9f82745caf82094a22f2329c1781764d
rryyii <ryanyi455@gmail.com>
2024-12-03 16:02:59 -0500

    Added delete functionality to playlists page; edited html and css for corresponding pages

--- scripts/viewPlaylist.js
+++ scripts/viewPlaylist.js
@@ -5,7 +5,14 @@ for (let i = 0; i < localStorage.length; i++) {
         if (localStorage.key(i) != "spotify_access_token") {
                 if(localStorage.key(i) != "debug") {
                         const playlist = document.createElement("li");
+                        const deleteButton = document.createElement("button");
+                        deleteButton.addEventListener("click", async () => {
+                                localStorage.removeItem(localStorage.key(i));
+                                location.reload();
+                        });
+                        deleteButton.classList.add("delete");
                         playlist.textContent = localStorage.key(i);
+                        playlist.append(deleteButton);
                         playlistDisplay.appendChild(playlist);
                 }
         }
